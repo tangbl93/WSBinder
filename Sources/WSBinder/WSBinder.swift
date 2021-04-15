2e0f7ff703b96a43634a0867a17e1f21e75255fa
tangbl93 <tangbl93@gmail.com>
2021-04-15 18:19:09 +0800

    fix

--- Sources/WSBinder/WSBinder.swift
+++ Sources/WSBinder/WSBinder.swift
@@ -7,7 +7,7 @@ import Foundation
 public class WSBinder<T: AnyObject>: NSObject {
     public typealias Action = (_ instance: T) -> Void
 
-    init(_ instance: T?) {
+    public init(_ instance: T?) {
         super.init()
         self.instance = instance
     }
